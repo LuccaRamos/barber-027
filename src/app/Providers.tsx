9d5c7a775a76eefef9750cf791231316d8974a7b
Lucas Ramos Marques <lucas@rdweb.com.br>
2023-08-31 23:12:31 -0300

    versao desktop ok

---
+++ src/app/Providers.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { ThemeProvider } from "@material-tailwind/react";
+import { ReactNode } from "react";
+
+
+export const Providers = ({
+    children,
+}: {
+    children: ReactNode
+}) => {
+    return (
+        <ThemeProvider>
+            {children}
+        </ThemeProvider>
+    )
+}
+
+export default Providers
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,14 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
+const withMT = require("@material-tailwind/react/utils/withMT");
 
-const config: Config = {
+const config: Config = withMT({
   content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -12,9 +16,10 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'header': "url('/img/header-bg.png')",
       },
     },
   },
   plugins: [],
-}
+})
 export default config
